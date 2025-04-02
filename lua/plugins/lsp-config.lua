7ef72ec61ad05914e183f9d7e8cbc33f4c22eae3
aaronjoslinwangdu <aaronjoslinwangdu@gmail>
2025-04-01 22:07:15 -0400

    add gitsigns + autotag, remove snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -72,6 +72,8 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>fr", vim.lsp.buf.references, {})
     end,
   },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,11 +1,16 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+
 vim.schedule(function()
   vim.opt.clipboard = "unnamedplus"
 end)
+
+vim.keymap.set("n", "<leader>Q", vim.cmd.copen, { silent = true })
+vim.keymap.set("n", "<leader>q", vim.cmd.cclose, { silent = true })
