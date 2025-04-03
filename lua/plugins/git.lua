e914f48f0b314f981093f88808b3644292e84bec
aaronjoslinwangdu <aaronjoslinwangdu@gmail>
2025-04-02 21:35:54 -0400

    add lazygit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,14 +1,33 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup({
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		})
-	end,
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "+" },
+          change = { text = "~" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+        },
+      })
+    end,
+  },
 }
