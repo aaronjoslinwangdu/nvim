fdbf5c68faf4444832a8e9719e25a4d55602c780
aaronjoslinwangdu <aaronjoslinwangdu@gmail>
2025-06-13 22:36:09 -0400

    swap to fzf-lua

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,28 @@
+return {
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("fzf-lua").setup({
+			oldfiles = {
+				cwd_only = true,
+				include_current_session = true,
+			},
+			grep = {
+				rg_glob = true,
+				glob_flag = "--iglob",
+			},
+			previewers = {
+				builtin = {
+					syntax_limit_b = 1024 * 100, -- 100KB
+				},
+			},
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>sf", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
+		vim.api.nvim_set_keymap("n", "<leader>sr", [[<Cmd>lua require"fzf-lua".oldfiles()<CR>]], {})
+		vim.api.nvim_set_keymap("n", "<leader>sb", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
+		vim.api.nvim_set_keymap("n", "<leader>sg", [[<Cmd>lua require"fzf-lua".grep()<CR>]], {})
+		vim.api.nvim_set_keymap("n", "fu", [[<Cmd>lua require"fzf-lua".lsp_references()<CR>]], {})
+		vim.api.nvim_set_keymap("n", "gd", [[<Cmd>lua require"fzf-lua".lsp_definitions()<CR>]], {})
+	end,
+}
